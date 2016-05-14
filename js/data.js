68d0ad7ac7b36ffdf0e5e7f43c0bcdecd867dc13
Blaine Schmeisser <blainesch@gmail.com>
2016-05-13 21:47:01 -0700

    Cache icons in repo, instead of hotlinking.

--- js/data.js
+++ js/data.js
@@ -1,41 +1,41 @@
 module.exports = {
   "results": [
     {
-      "icon": "http://icons.iconarchive.com/icons/wineass/ios7-redesign/256/Calculator-icon.png",
+      "icon": "icons/calculator.png",
       "title": 6,
       "subtitle": "Select item to copy the value to the clipboard."
     },
     {
-      "icon": "http://www.hmlglaw.com/uploads/images/amazon-icon.png",
+      "icon": "icons/amazon.png",
       "title": "Search Amazon for 'batteries'"
     },
     {
-      "icon": "https://blog.agilebits.com/wp-content/uploads/2014/04/1Pi-icon-1024.png",
+      "icon": "icons/1password.png",
       "title": "Chase",
       "subtitle": "Login to https://login.chase.net/login?r=0cc175b9c0f1b6a831c399e269772661187ef4436122d1cc2f40dc2b92f0eba0900150983cd24fb0d6963f7d28e17f72900150983cd24fb0d6963f7d28e17f72ab56b4d92b40713acc5af89985d4b786e80b5017098950fc58aad83c8c14978e0cc175b9c0f1b6a831c399e269772661187ef4436122d1cc2f40dc2b92f0eba0900150983cd24fb0d6963f7d28e17f72900150983cd24fb0d6963f7d28e17f72ab56b4d92b40713acc5af89985d4b786e80b5017098950fc58aad83c8c14978e0cc175b9c0f1b6a831c399e269772661187ef4436122d1cc2f40dc2b92f0eba0900150983cd24fb0d6963f7d28e17f72900150983cd24fb0d6963f7d28e17f72ab56b4d92b40713acc5af89985d4b786e80b5017098950fc58aad83c8c14978e"
     },
     {
-      "icon": "http://orig10.deviantart.net/bfa2/f/2014/166/9/1/adobe_photoshop_icon_v2_by_dudekpro-d7mg13s.png",
+      "icon": "icons/photoshop.png",
       "title": "Adobe Photoshop CC 2015",
       "subtitle": "/Applications/Adobe CC 2015/Adobe Photoshop CC 2015.app"
     },
     {
-      "icon": "https://cdn1.iconfinder.com/data/icons/logotypes/32/chrome-512.png",
+      "icon": "icons/chrome.png",
       "title": "Emoji",
       "subtitle": "http://www.emoji-cheat-sheet.com/"
     },
     {
-      "icon": "http://image005.flaticon.com/25/png/512/25/25231.png",
+      "icon": "icons/github.png",
       "title": "Gist: Create from clipboard",
       "subtitle": "Optionally specify options"
     },
     {
-      "icon": "http://icons.iconarchive.com/icons/alecive/flatwoken/512/Apps-Pandora-icon.png",
+      "icon": "icons/pandora.png",
       "title": "Next",
       "subtitle": "Next song in Pandora"
     },
     {
-      "icon": "http://cdn.shopify.com/s/files/1/0185/5092/products/objects-0177.png?v=1369543387",
+      "icon": "icons/beer.png",
       "title": "Beer",
       "subtitle": "Select to copy emoji to clipboard!"
     }
